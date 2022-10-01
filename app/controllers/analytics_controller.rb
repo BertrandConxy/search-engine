5e647f51c08800510eb21df74f40b3a39e8cec4e
bertrand <mutanganabertrand@gmail.com>
2022-10-01 12:38:51 +0200

    Debug analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,7 +1,7 @@
 class AnalyticsController < ApplicationController
     def index
-        @grouped_searches = Analytic.group(:query).pluck('query, count(query) as COUNT').sort_by do |a|
-          a[1]
-        end.reverse.first(100)
+      @grouped_analytics = Analytic.group(:query).pluck('query, count(query) as COUNTER').sort_by do |a|
+        a[1]
+      end.reverse.first(10)
     end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
     def save_search(query, session)
       return if query.nil? || query.length < 3
   
-      recent_search = Analytic.new(user_params)
+      recent_search = Analytic.new(query: query)
       recent_search.session_id = session
       session_search = Analytic.where(session_id: session).last
       if session_search.nil? || !session_search.searched?(query)
@@ -28,11 +28,9 @@ class ArticlesController < ApplicationController
       elsif session_search.query.length < query.length
         session_search.update(query: query)
       end
+    
     end
-  
-    def user_params
-      params.permit(:query)
-    end
+
   
     def check_session
       session[:user_id] = SecureRandom.urlsafe_base64(16) if session[:user_id].nil?
